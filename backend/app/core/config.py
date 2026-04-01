6c96f8583aaa8ceadf0bb5756be3f4c657f87dac
D3XTRO <mirazopablo@gmail.com>
2026-04-01 02:40:34 -0300

    feat(sub_mp): Implementacion de credenciales necesarias para DB
    Simplemente lo que hice aca fue hacer una implementacion de las credenciales necesarias para la base
    de datos
    Tambien modifique el requirements para tener bien definido las versiones de cada dependencia que
    vamos a usar, lo cual es imperioso tener y que, por si alguna fatalidad ocurre, tenemos las
    versiones especificas (Hail Prats)
    Tambien ya deje definida la dependencia de MercadoPago, asique este seria el primer commit de eso
    
    sub_mp #3

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,9 @@
 from pydantic_settings import BaseSettings
 from typing import Optional
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Carga las variables de entorno desde el archivo .env
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Whisper SaaS MVP"
@@ -9,6 +13,19 @@ class Settings(BaseSettings):
     R2_SECRET_ACCESS_KEY: str = ""
     RUNPOD_API_KEY: str = ""
     RUNPOD_ENDPOINT_ID: str = ""
+
+    # Database
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASS: str = os.getenv("DB_PASS")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: int = int(os.getenv("DB_PORT"))
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_SCHEMA: str = os.getenv("DB_SCHEMA")
+    DB_DRIVER: str = os.getenv("DB_DRIVER")
+    
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?options=-csearch_path%3D{self.DB_SCHEMA}"
 
     # Email
     EMAILS_ENABLED: bool = False
--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,1 +1,14 @@
-#future implementation of database connection and session management
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from ..core.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL, 
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False, 
+    autoflush=False, 
+    bind=engine
+)
