a7f2e8fd1b921e101c63c164d2e92e2b5b936cc7
D3XTRO <mirazopablo@gmail.com>
2026-04-06 17:43:52 -0300

    chore(routers): Grupos duplicados y entrypoint para uvicorn
    @fede-caballero , ojo aca, debido a que tenia que hacer las pruebas del endpoint para saber si
    respondian desde swagger agregue el entrypoint de uvicorn, asique si vas a reconstruir el docker,
    seguramente vas a tener un quilombito.
    Ah y removi grupos duplicados
    
    sub_mp #3

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,3 +27,8 @@ app.include_router(plan_router, prefix="/api/v1", tags=["Plans"])
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True, reload=True)
+
